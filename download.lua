0e2570cda62c2a8b05c3825b6e223eed0d902b8d
Master-Guy <566429+Master-Guy@users.noreply.github.com>
2023-11-03 16:00:40 +0100

    Download script

---
+++ download.lua
@@ -0,0 +1,5 @@
+local dialer = http.get("https://raw.githubusercontent.com/Master-Guy/StargateJourney/master/startup.lua").readAll()
+local dialerFile = fs.open("startup.lua", "w")
+dialerFile.write(dialer)
+dialerFile.close()
+shell.run("startup.lua")
