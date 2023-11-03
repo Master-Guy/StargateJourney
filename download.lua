3ed0d6ea62921a0e6070713d49f6e08889aab0a9
Master-Guy <566429+Master-Guy@users.noreply.github.com>
2023-11-03 16:01:41 +0100

    Pastebin line

--- download.lua
+++ download.lua
@@ -1,3 +1,4 @@
+-- pastebin run 8axhnygN
 local dialer = http.get("https://raw.githubusercontent.com/Master-Guy/StargateJourney/master/startup.lua").readAll()
 local dialerFile = fs.open("startup.lua", "w")
 dialerFile.write(dialer)
