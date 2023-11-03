5f92e37c723693e697a60ec1d34ed62452544fbf
Master-Guy <566429+Master-Guy@users.noreply.github.com>
2023-11-03 16:36:28 +0100

    Move comments out of the call

--- config.lua
+++ config.lua
@@ -1,17 +1,20 @@
+--[[
+    rotationModes:
+        0 = Always CW
+        1 = Always CCW
+        2 = Alternating
+        3 = Fastest
+    Debugging:
+        Prints extra messages
+    MaxTicksOpen:
+        Automatically close the gate, 200 = 10sec
+]]--
+
 settings.define("sg.config", {
     description = "Configuration for the Stargate Journeys dialer",
     default = {
-        -- rotationModes:
-        -- 0 = Always CW
-        -- 1 = Always CCW
-        -- 2 = Alternating
-        -- 3 = Fastest
         ["rotationMode"] = 3,
-
-        -- Debugging prints extra messages
         ["debugging"] = false,
-
-        -- Automatically close the gate, 200 = 10sec
         ["maxTicksOpen"] = 200
     },
     type = "table"
