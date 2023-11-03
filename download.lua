4d7b4abade7e73d4f9a3ff8ec7df415f6d22f6e5
Master-Guy <566429+Master-Guy@users.noreply.github.com>
2023-11-03 16:47:17 +0100

    Adding a delete for startup.lua

--- download.lua
+++ download.lua
@@ -1,4 +1,6 @@
 -- pastebin run 8axhnygN
+fs.delete("startup.lua")
+
 local dialer = http.get("https://raw.githubusercontent.com/Master-Guy/StargateJourney/master/startup.lua").readAll()
 local dialerFile = fs.open("startup.lua", "w")
 dialerFile.write(dialer)
